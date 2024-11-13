4e858298d337893dda46832cba85aec6b1fd4d62
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2024-11-13 09:08:39 -0500

    Init

---
+++ src/info.ts
@@ -0,0 +1,18 @@
+// import Battery from "./battery";
+import { VolumeIcon } from "./volume";
+// import { BrightnessIcon } from "./brightness";
+// import { Separator } from "./separator";
+
+export default function() {
+  return Widget.Box({
+    class_name: "info",
+    spacing: 5,
+    children: [
+      // Battery(),
+      // Separator(),
+      VolumeIcon(),
+      // BrightnessIcon(),
+    ]
+  });
+}
+
---
+++ src/volume.ts
@@ -0,0 +1,40 @@
+import Gtk from "gi://Gtk?version=3.0";
+import { Stream } from "resource:///com/github/Aylur/ags/service/audio.js";
+
+const audio = await Service.import("audio");
+
+function get_vol_icon(speaker: Stream): string {
+  if (speaker?.is_muted ?? true) {
+    return `\udb81\udd81`;
+  }
+  const vol = (speaker?.volume ?? 0) * 100;
+  const icon_list: [number, string][] = [
+    [101, `\udb81\udf5d`],
+    [67, `\udb81\udd7e`],
+    [34, `\udb81\udd80`],
+    [5, `\udb81\udd7f`],
+    [0, `\udb83\ude08`],
+  ];
+  const icon = icon_list.find(([thres, _]) => thres <= vol)?.[1];
+  return icon ?? `\udb83\ude08`;
+}
+
+export function VolumeIcon() {
+  return Widget.Box({
+    className: "volume",
+    valign: Gtk.Align.CENTER,
+    tooltip_text: audio.bind("speaker").as(s => `Volume ${Math.floor((s?.volume ?? 0) * 100)}%`),
+    spacing: 5,
+    children: [
+      Widget.Label({
+        valign: Gtk.Align.CENTER,
+        label: audio.bind("speaker").as(s => get_vol_icon(s)),
+      }),
+      Widget.ProgressBar({
+        valign: Gtk.Align.CENTER,
+        vertical: false,
+        value: audio.bind("speaker").as(s => s?.volume ?? 0),
+      }),
+    ],
+  });
+}
